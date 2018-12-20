2ee3e1ffc452acb228783fe74ab759e6d06bd916
paulamanz <pamaiz@gmail.com>
2018-12-20 18:28:49 +0100

    Hopefully this works for Niels

--- app/src/main/java/es/upm/hcid/pui/assignment/Article.java
+++ app/src/main/java/es/upm/hcid/pui/assignment/Article.java
@@ -28,7 +28,10 @@ public class Article extends ModelEntity{
 
 
 	private String parseStringFromJson(JSONObject jsonArticle, String key, String def){
-		Object in = jsonArticle.getOrDefault(key,def);
+		Object in = null;
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+			in = jsonArticle.getOrDefault(key,def);
+		}
 		return (in==null?def:in).toString();
 	}
 
