83a9ec8800da0a5fa59b655eafe1a2f5085e3511
Niels <nielsdikken@gmail.com>
2019-01-09 21:17:41 +0100

    News Details layout + image picker

--- app/src/main/java/com/eit/paulamanz/firstapp/AboutUs.java
+++ app/src/main/java/com/eit/paulamanz/firstapp/AboutUs.java
@@ -4,17 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Calendar;
 
 public class AboutUs extends AppCompatActivity {
 
